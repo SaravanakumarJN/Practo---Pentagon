92eb315823db9fb00df8861d3e9326ff18f6407f
SaravanakumarJN <kumarjnsaravana@gmail.com>
2021-03-12 13:11:44 +0530

    login/register - persisted

--- practo_clone/src/routes/Routes.jsx
+++ practo_clone/src/routes/Routes.jsx
@@ -19,8 +19,6 @@ const Routes = () => {
                 <Footer/>
             </Route>
             <Route exact path = "/">
-                <Landingpage/>
-                {/* <Bookingdetails/> */}
                 <Navbar/>
                 <Landingpage/>
                 <Footer/>
